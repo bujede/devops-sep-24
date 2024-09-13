fd64d71965b6a5e95c0ccfdcd98e69bd2fd67416
Bilal Ujede <83216230+bujede@users.noreply.github.com>
2024-09-13 08:36:49 +0530

    Initial commit

---
+++ python/list/example-01.py
@@ -0,0 +1,21 @@
+score1 = "DevOps"
+score2 = "Araini"
+
+if score1 > score2:
+    print('A > B')
+elif score1 == score2:
+    print('A = B')
+elif score1 != score2:
+    print('A != B')
+elif score1 >= score2:
+    print('A >= B')
+elif score1 <= score2:
+    print('A <= B')
+elif score1 < score2: # else if (<condition>)
+    print('A < B')
+else:
+    print('A < B')
+
+
+# Count the bytes
+# Error
